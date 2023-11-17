a1157c06499ffcde9aef1e95a1fb5a897ebd78e0
Bartek <bartek.maronka@gmail.com>
2023-11-17 10:02:00 +0100

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:ecommerce_app/firebase_options.dart';
 import 'package:ecommerce_app/src/app_bootstrap.dart';
 import 'package:ecommerce_app/src/app_bootstrap_firebase.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:go_router/go_router.dart';
@@ -8,7 +10,9 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   usePathUrlStrategy();
   GoRouter.optionURLReflectsImperativeAPIs = true;
-  // TODO: Initialize firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   final appBootstrap = AppBootstrap();
   final container = await createFirebaseProviderContainer();
